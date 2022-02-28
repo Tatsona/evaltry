b46238897cb88efe63210341e88667f4a0ca2b13
tatson <tatson@gmail.com>
2022-02-28 20:11:11 +0530

    first commit

---
+++ pydev/addmul.py
@@ -0,0 +1,6 @@
+print('Program to display addition and multiplication of 2 nos')
+num1 = int(input('Please enter number1: '))
+num2 = int(input('Please enter number2: '))
+
+print(f'Addition of {num1} and {num2} = {num1+num2}')
+print(f'Multiplication of {num1} and {num2} = {num1*num2}')
